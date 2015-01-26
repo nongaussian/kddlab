caa6630b277ac546be7b4d2789edd969db105977
Younghoon Kim <nongaussian@gmail.com>
2015-01-26 17:56:35 +0900

    a more test file

---
+++ graphmatch/src/utils/HelloUtil.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class HelloUtil {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello, utils!");
+	}
+
+}
